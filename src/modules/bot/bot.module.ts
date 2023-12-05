9a01ca46ff096686a88725df928ad97b29344fdf
ibah <starlord77@mail.ru>
2023-12-05 21:48:08 +0300

    refactor(bot-module): удалил лишнее из модуля бота

--- src/modules/bot/bot.module.ts
+++ src/modules/bot/bot.module.ts
@@ -1,31 +1,20 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { Users } from '../database/local.database/models/User.model';
-import { BotService } from './bot.service';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { session } from 'telegraf';
 import { ScenesModule } from 'src/pages/scenes/scenes.module';
-
-const sessionMiddleWare = session();
+import { sessionMiddleWare } from 'src/utils/sessionMiddleware';
 
 @Module({
   imports: [
-    SequelizeModule.forFeature([Users], 'local'),
     TelegrafModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => ({
         token: config.get<string>('bot.token'),
-        /**
-         * Чтобы бот работал, надо добавить мидлвэйр
-         * и модуль сцен
-         */
         middlewares: [sessionMiddleWare],
         include: [ScenesModule],
       }),
       inject: [ConfigService],
     }),
   ],
-  providers: [BotService],
 })
 export class BotModule {}
