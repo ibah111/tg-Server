2134c446dad0760bf6cad4b75ee319eb99d3e620
ibah <109962513+ibah111@users.noreply.github.com>
2025-07-29 13:25:25 +0300

    fix: fixed user repo

--- src/databases/sqlite.database/repository/user.repository.ts
+++ src/databases/sqlite.database/repository/user.repository.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Users } from '../models/User.model';
 import { FindOptions } from 'sequelize';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
 export default class UserRepository {
-  constructor(private readonly modelUser: typeof Users) {}
+  constructor(
+    @InjectModel(Users, 'local')
+    private readonly modelUser: typeof Users,
+  ) {}
 
   async createUser(user: Users) {
     return await this.modelUser.create(user);
