5868ac01301c31ab1133f4eeea31038fab9b2583
ibah <109962513+ibah111@users.noreply.github.com>
2025-07-29 11:26:00 +0300

    feat: removed cron

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import DatabaseModule from '../databases/database.module';
-import { CronModule } from './cron/cron.module';
 import IpModule from './ip/ip.module';
 import { CacheModule } from './cache/cache.module';
 import { GptModule } from './gpt/gpt.module';
 
 @Module({
-  imports: [DatabaseModule, CronModule, IpModule, CacheModule, GptModule],
+  imports: [DatabaseModule, IpModule, CacheModule, GptModule],
 })
 export class ModuleOfModules {}
